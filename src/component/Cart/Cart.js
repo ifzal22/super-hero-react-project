ad280bd4afa9e2f07dd2929706a1cde404349138
ifzal22 <enjoyyourlife2244@gmailm>
2021-09-26 16:16:01 +0600

    push

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.css'
 const Cart = (props) => {
   
 
 
+const [autor, setAutor] = useState([]);
+useEffect(()=>{
+    fetch('./manifest.json')
+    .then(res => res.json())
+    .then(data => setAutor(data))
+},[])
 
 
-
-
+const  handAddcard = (auto) =>{
+    const newName = [...autor, auto];
+    setAutor(newName);
+}
 
     
     const {cart} = props;
@@ -18,15 +26,20 @@ const name = (previous, cart)=> previous + cart.name;
   let total = cart.reduce(totalReduce, 0);
 
   let NAME = cart.reduce(name, 0)
- 
+ console.log(NAME)
  
   
   
     return (
         <div className="Cart">
+
+            {
+                autor.map(auto => <p>{}</p>
+                    )
+            }
              <h1>Person: {props.cart.length}  </h1>
              <p>Name: {
-                    <ul>  <li>   {NAME}  </li></ul>
+                    <p>{NAME} </p>
                       } </p>
        
             <h3>Selary: {total} </h3>
--- src/component/Mans/Mans.js
+++ src/component/Mans/Mans.js
@@ -3,10 +3,17 @@ import './Mans.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faShoppingCart,  } from '@fortawesome/free-solid-svg-icons'
 
+import Rating from 'react-rating';
+
 const Mans = (props) => {
     console.log(props.man)
     const {name,Age,img, sellarry,company,about,star} = props.man
     const element = <FontAwesomeIcon icon={ faShoppingCart} />
+   
+   
+
+   
+  
     console.log(sellarry,name)
     return (
         <div className="man-document">
@@ -19,18 +26,28 @@ const Mans = (props) => {
             <h1>{name}</h1>
             <h5>Age: {Age}</h5>
             <h5>Salary: {sellarry} </h5>
+<p className="star">
+<Rating readonly
+ initialRating={star}
+ fullSymbol="fas fa-star fa-2px"
+ emptySymbol="far fa-star fa-2px">
+   </Rating>
+</p>
+
             <h5>Company:  {company}</h5> 
 
 
-
-
             <p><span className="about"> About:  </span> {about}</p>
-
-            
-
-
+            <FontAwesomeIcon icon={["far", "coffee"]} />
+   
+ 
 <button onClick={()=>props.handAddcard(props.man)} className="BTN"> {element} add card</button>
    
+
+<div className="icon">
+<i class="fab fa-facebook-square"></i>
+<i class="fab fa-youtube"></i>
+</div>
   </div>
 
 
